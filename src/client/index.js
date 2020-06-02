896a7a31315333a90b451963c5c3b23d78117490
nuekleer <matthewrchristensen@gmail.com>
2020-06-02 00:31:55 -0500

    sass lesson complete

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,10 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/header.scss'
+import './styles/form.scss'
+import './styles/footer.scss'
 
 console.log(checkForName);
 
